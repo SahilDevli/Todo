aa690e0bf165e951f2da4bb0c67b0dc51051648f
sahildevli <sahildevli07@gmail.com>
2026-07-01 21:03:18 +0530

    initiaizing todo app from scratch

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    hello
+    <input type="text" />
+    </>
+  )
+}
+
+export default App
